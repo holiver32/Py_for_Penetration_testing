5a4c29d8c7d942018bd030f98b022260178dfafd
holiver32 <holiver.hurtado7@gmail.com>
2025-06-29 19:43:43 +0200

    modification in client ip

--- TCP_client.py
+++ TCP_client.py
@@ -5,7 +5,7 @@ clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host = socket.gethostname()
 port = 444
 
-clientsocket.connect(('192.168.1.104', port))
+clientsocket.connect(('192.168.56.1', port))
 
 message = clientsocket.recv(1024)
 
