7577f3ca0b2d1c5748f5fdea6118452e27429876
holiver32 <holiver.hurtado7@gmail.com>
2025-06-29 19:39:34 +0200

    Create TCP server and client.

---
+++ TCP_server.py
@@ -0,0 +1,24 @@
+import socket
+
+#Creating the socket
+serversocket - socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = socket.gethostbyname()
+port = 444
+
+#Binding to socket
+seversocket.bind(('192.168.56.1', port)) #Host will be replaced/substitued with IP, if changed and not running on host.
+
+#Starting TCP listener
+seversocket.listen(3)
+
+while True:
+    #Starting the connection
+    clientsocket, address = seversocket.accept()
+
+    print("received connection from %s " % str(address))
+
+    message = 'hello! thank you for connecting to the server' + "\r\n"
+    clientsocket.send(message.encode('ascii'))
+
+    clientsocket.close()
